4f71c6c669ebf08fea05ed6c915bbebfbbecb537
Vrok2401 <52956263+Vrok2401@users.noreply.github.com>
2020-07-16 12:43:01 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ const { response } = require('express');
 
 const apikey = 'aXROVGlucUdKMG1SbUk2eDJIUHZYUT09';
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static('client/public'));
-}
+app.use(express.static(path.join(__dirname,'./client/build')));
 
 app.get('/', (req, res) => {
-    const index = path.join(__dirname,'./client/public/index.html');
-    console.log(index);
-    res.sendFile(index);
+    const index = path.join(__dirname,'./client/build/index.html');
+    
+    res.sendFile(index, function(err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
 });
 
 //Get records
@@ -33,16 +35,23 @@ app.get('/getallplants', (req, res) => {
 app.get('/search', (req, res) => {
     type = req.query.type;
     name = req.query.name;
-    const querystr = `https://trefle.io/api/plants?${type}=${name}&token=${apikey}`;
+
+    var querystr;
+    if (type == 'q') {
+        querystr = `https://trefle.io/api/v1/plants/search?token=${apikey}&q=${name}`;
+    } else {
+        querystr = `https://trefle.io/api/v1/plants?token=${apikey}&filter[${type}]=${name}&order[scientific_name]=asc`;
+    }
+    
     console.log(type+" search: "+name);
+    console.log(querystr);
     axios.get(querystr)
     .then( (response) => {
-        if (!response.data || response.data === undefined) {
-            res.status(200).send('Plant not found');
-            return;
+        if (!response.data || response.data === undefined || response.data.meta.total === 0) {
+            console.log("Search: No plants found");
+            res.status(200).send('');
         } else {
-            console.log(response.data);
-            console.log("Search found");
+            console.log("Search: "+response.data.meta.total+" result(s) found");
             res.status(200).send(response.data);
         }
     })
@@ -53,39 +62,64 @@ app.get('/search', (req, res) => {
 });
 //Add record
 app.get('/add', (req, res) => {
-    var url = req.query.link+`?token=${apikey}`;
+    var url = `https://trefle.io`+req.query.link+`?token=${apikey}`;
     console.log(url);
 
     axios.get(url)
     .then( (response)  => {
-        commonName = "N/A";
-        if (response.data.common_name != null) { 
-            //capitalise first letter of common name
-            commonName = response.data.common_name.charAt(0).toUpperCase()+response.data.common_name.slice(1);
-        }
-        image_url = null;
-        if (response.data.images !== undefined) {
-            image_url = response.data.images[0].url;
-        }
-        plantValue = new Plant ({
-            common_name: commonName,
-            scientific_name: response.data.scientific_name,
-            genus: response.data.genus.name,
-            family: response.data.family.name,
-            order: response.data.order.name,
-            class: response.data.class.name,
-            division: response.data.division.name,
-            image: image_url
-        });
-            
-        plantValue.save()
-        .then(result=> {
-            console.log("Success: "+result);
-            res.status(200).json("Plant added");
-        })
-        .catch(error=> {
+        data = null;
+        //console.log(response.data.data);
+        if (response.data.data == undefined) {
             res.status(400).send(error);
             console.log("Error: "+error);
+        } else {
+            data = response.data.data;
+        }
+        //console.log(data);
+        common_name = "N/A";
+        if (data.common_name != null) { 
+            //capitalise first letter of common name
+            common_name = data.common_name.charAt(0).toUpperCase()+data.common_name.slice(1);
+        }
+        scientific_name = data.scientific_name;
+        genus = data.genus.name;
+        family = data.family.name;
+        image_url = null;
+        if (data.image_url !== undefined) {
+            image_url = data.image_url;
+        }
+
+        //get order and above data through family link
+        var family_url = `https://trefle.io`+data.family.links.self+`?token=${apikey}`;
+        axios.get(family_url)
+        .then( (response)  => {
+            console.log('Retrieving order data');
+            var family_data = response.data.data;
+            plantValue = new Plant ({
+                common_name: common_name,
+                scientific_name: scientific_name,
+                genus: genus,
+                family: family,
+                order: family_data.division_order.name,
+                class: family_data.division_order.division_class.name,
+                division: family_data.division_order.division_class.division.name,
+                image: image_url
+            });
+
+            //save plant record
+            plantValue.save()
+            .then(result=> {
+                console.log("Success: "+result);
+                res.status(200).json("Plant added");
+            })
+            .catch(error=> {
+                res.status(400).send(error);
+                console.log("Error: "+error);
+            });
+        })
+        .catch(error => {
+            res.status(400).send(error);
+            console.log(error);
         });
     })
     .catch(error => {
